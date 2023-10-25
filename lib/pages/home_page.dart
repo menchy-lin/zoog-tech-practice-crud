abd075dd108997107d382973c674022baf1d30bf
menchy-lin <menchymarmol@gmail.com>
2023-10-25 17:11:04 +0800

    Added To Do List CRUD Operations

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:zoog_flutter_crud/db/sql_helper.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // All journals
+  List<Map<String, dynamic>> _journals = [];
+
+  bool _isLoading = true;
+
+  // Function - fetch all data from the database
+  void _refreshJournals() async {
+    final data = await SQLHelper.getItems();
+
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshJournals(); // Loads the diary when the app starts
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  // Function - floating button will be triggered when pressed, and also when you want to update an item
+  void _showForm(int? id) async {
+    if (id != null) {
+      // id == null means create a new item
+      // id != null means update an existing item
+      final existingJournal =
+          _journals.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+
+    showModalBottomSheet(
+      context: context,
+      elevation: 5,
+      isScrollControlled: true,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          top: 15,
+          left: 15,
+          right: 15,
+          // this will prevent the soft keyboard from covering the text fields
+          bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+        ),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(hintText: 'Title'),
+            ),
+            TextField(
+              controller: _descriptionController,
+              decoration: const InputDecoration(hintText: 'Description'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // Save new journal
+                if (id == null) {
+                  await _addItem();
+                }
+
+                if (id != null) {
+                  await _updateItem(id);
+                }
+
+                // Clear the text fields
+                _titleController.text = '';
+                _descriptionController.text = '';
+
+                // Close the bottom sheet
+                if (!mounted) return;
+                Navigator.of(context).pop();
+              },
+              child: Text(id == null ? "Create New" : "Update"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Insert a new journal to the database
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // Update an existing journal
+  Future<void> _updateItem(int id) async {
+    await SQLHelper.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // To access BuildContext across async function
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  // Delete an item
+  void _deleteItem(int id) async {
+    await SQLHelper.deleteItem(id);
+
+    scaffoldMessengerKey.currentState?.showSnackBar(const SnackBar(
+      content: Text('Successfully deleted a journal!'),
+    ));
+    _refreshJournals();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'CRUD Operations',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 90, 2, 179),
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _journals.length,
+              itemBuilder: (context, index) => Card(
+                color: const Color.fromARGB(255, 53, 20, 103),
+                margin: const EdgeInsets.all(15),
+                child: ListTile(
+                  title: Text(
+                    _journals[index]['title'],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  subtitle: Text(
+                    _journals[index]['description'],
+                    style: const TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  trailing: SizedBox(
+                    width: 100,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit, color: Colors.white),
+                          onPressed: () => _showForm(_journals[index]['id']),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.white),
+                          onPressed: () => _deleteItem(_journals[index]['id']),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(null),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
